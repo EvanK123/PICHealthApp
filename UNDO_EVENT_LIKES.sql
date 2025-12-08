1654bc1fba900aaa1dd45d237b9bba70c6cf6e49
EvanK123 <42948168+EvanK123@users.noreply.github.com>
2025-12-08 15:09:30 -0800

    Delete UNDO_EVENT_LIKES.sql

--- UNDO_EVENT_LIKES.sql
+++
@@ -1,5 +0,0 @@
--- Undo event_likes table creation
-DROP TABLE IF EXISTS event_likes CASCADE;
-
--- Remove from realtime publication (if it was added)
-ALTER PUBLICATION supabase_realtime DROP TABLE IF EXISTS event_likes;
